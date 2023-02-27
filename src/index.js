3fdf3f9f9c2b006d8664d7095f5a1b1cffb072cd
ulianava <uliana.averianova@gmail.com>
2023-02-27 16:22:33 +0200

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  if(!(str.length % 2 == 0)){
+    return false
+  }
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.forEach(brackets => {
+      brackets = brackets.join('');
+      if(str.includes(brackets)){
+        str = str.replace(brackets,'')
+        i = 0;
+      }
+    });
+  } 
+  return str.length ? false : true;
+
 }
